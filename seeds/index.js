28f4ec4bbebe025c99248798e1c5ec4440ae5d09
Wilson Zheng <wjz224@lehigh.edu>
2024-08-23 18:21:43 -0400

    Added ways to generate test data for findcamp

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {descriptors, places} = require('./seedHelpers');
+
+// Connect to mongo database
+mongoose.connect('mongodb://127.0.0.1:27017/find-camp', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() =>{
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) =>{
+        console.log(`Error connecting to MongoDB: ${err}`);
+    });
+
+// Function to randomly get a value from the array passed in
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// Generate random campgrounds based on data found stored in models/campground and seedHelpers
+const seedDB = async() =>{
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        camp.save();
+
+    }
+}
+
+// Close connection after finishing generating the seeds.
+seedDB().then(() =>{
+    mongoose.connection.close();
+})
