503065ee78ec69f7a13c3c6cf6ac33c08f0369ed
Wilson Zheng <wjz224@lehigh.edu>
2024-08-23 18:49:04 -0400

    Added route to add campgroudns

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/find-camp', {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) =>{
     res.render("home");
 })
@@ -27,11 +29,21 @@ app.get('/campgrounds', async (req, res) =>{
     res.render('campgrounds/index', {campgrounds});
 });
 
+app.get('/campgrounds/new', (req, res) =>{
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) =>{
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground.id}`);
+});
 app.get('/campgrounds/:id', async(req, res) =>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', {campground});
 });
+
 app.get('/makecampground', async (req, res) => {
     const camp = new Campground ({title: 'My Backyard', description: 'cheap camping'});
     await camp.save();
