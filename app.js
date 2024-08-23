ec54962f85b183e5294e1df6f90c31ee430662bb
Wilson Zheng <wjz224@lehigh.edu>
2024-08-23 17:58:23 -0400

    Basic express setup for FindCamp

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render("home");
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000!")
+});
