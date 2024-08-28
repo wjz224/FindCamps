ab311c63e617f11e547ed8f5a46b88f412bd4ae6
Wilson Zheng <wjz224@lehigh.edu>
2024-08-28 12:44:02 -0400

    added styles to adding new campground and editing existing campground forms

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.post('/campgrounds', async (req, res) =>{
 app.get('/campgrounds/:id', async(req, res) =>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
+    console.log(campground);
     res.render('campgrounds/show', {campground});
 });
 // PUT Route for updating individual campgrounds 
--- seeds/index.js
+++ seeds/index.js
@@ -23,11 +23,15 @@ const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Random Description" ,
+            price: price,
         });
-        camp.save();
+        await camp.save();
 
     }
 }
