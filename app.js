8be1f11da80f2ff8b600a50f12f3c0ef8e373fbb
Wilson Zheng <wjz224@lehigh.edu>
2024-10-07 01:58:41 -0400

    Added authentication using passport. Also made user experience better by incorporating persistence

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const flash = require('connect-flash');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews')
+const campgroundRoutes = require('./routes/campgrounds');
+const userRoutes = require('./routes/users')
+const reviewRoutes = require('./routes/reviews')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 // Connect to mongo database
 mongoose.connect('mongodb://127.0.0.1:27017/find-camp', {
@@ -49,18 +53,28 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 app.use((req,res,next) =>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
+
 app.get('/', (req, res) =>{
     res.render("home");
 })
-
-app.use('/campgrounds', campgrounds);
-
-app.use('/campgrounds/:campId/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:campId/reviews', reviewRoutes);
 
 app.all(/(.*)/, (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,8 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground')
 const {campgroundSchema} = require('../schemas.js');
-
+const {isLoggedIn} = require('../middleware')
+;
 const validateCampground = (req, res, next) =>{
     const {error} = campgroundSchema.validate(req.body);
     if(error){
@@ -22,11 +23,11 @@ router.get('/', catchAsync(async (req, res) =>{
     res.render('campgrounds/index', {campgrounds});
 }));
 // GET Route for making new campgrounds form
-router.get('/new', (req, res) =>{
+router.get('/new', isLoggedIn, (req, res) =>{
     res.render('campgrounds/new');
 });
 // POST Route for posting new campgrounds
-router.post('/', validateCampground, catchAsync(async (req, res, next) =>{
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) =>{
     const newCampground = new Campground(req.body.campground);
     await newCampground.save();
     req.flash('success', 'Successfully made a new campground!');
@@ -59,7 +60,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) =>{
     res.redirect(`/campgrounds/${updatedCampground.id}`);
 }));
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground!');
